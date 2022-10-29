f4daf9ef1e3bbd04a6695b8c57bc55f5112659b8
JohnnyReynaga <u201814271@upc.edu.pe>
2022-10-28 19:35:54 -0500

    Creacion del UbicacionService

--- src/main/java/pe/edu/upc/demotopstorefinder/serviceinterfaces/IUbicacionService.java
+++ src/main/java/pe/edu/upc/demotopstorefinder/serviceinterfaces/IUbicacionService.java
@@ -1,12 +1,18 @@
 package pe.edu.upc.demotopstorefinder.serviceinterfaces;
 
+import pe.edu.upc.demotopstorefinder.entities.TipoPago;
 import pe.edu.upc.demotopstorefinder.entities.Ubicacion;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUbicacionService {
 
     public void insert(Ubicacion ubicacion);
 
     List<Ubicacion>list();
+
+    public void delete(int idUbicacion);
+
+    Optional<Ubicacion> ListarId(int idUbicacion);
 }
