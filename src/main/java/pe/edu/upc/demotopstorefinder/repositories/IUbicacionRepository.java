4c852ed7f45f9129d8df1cb983ac7fcabcf3330e
JohnnyReynaga <u201814271@upc.edu.pe>
2022-10-28 19:37:10 -0500

    Creacion del Ubicacion Repository

--- src/main/java/pe/edu/upc/demotopstorefinder/repositories/IUbicacionRepository.java
+++ src/main/java/pe/edu/upc/demotopstorefinder/repositories/IUbicacionRepository.java
@@ -1,8 +1,13 @@
 package pe.edu.upc.demotopstorefinder.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pe.edu.upc.demotopstorefinder.entities.TipoPago;
 import pe.edu.upc.demotopstorefinder.entities.Ubicacion;
+
+import java.util.List;
 
 @Repository
 public interface IUbicacionRepository extends JpaRepository<Ubicacion,Integer> {
