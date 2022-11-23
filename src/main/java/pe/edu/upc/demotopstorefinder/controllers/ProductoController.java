b8a0befe7148c47a1a10359fec010632730fbc30
RobertoCaceresRivas <113564581+RobertoCaceresRivas@users.noreply.github.com>
2022-11-22 21:30:31 -0500

    Revert "QUERY DE CANTIDAD DE PRODUCTOS EN TIENDAS"
    This reverts commit 4b047424e811b91e4eeb7bf4f21f0757f7db5e79.

--- src/main/java/pe/edu/upc/demotopstorefinder/controllers/ProductoController.java
+++ src/main/java/pe/edu/upc/demotopstorefinder/controllers/ProductoController.java
@@ -3,9 +3,6 @@ package pe.edu.upc.demotopstorefinder.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.demotopstorefinder.entities.Producto;
-import pe.edu.upc.demotopstorefinder.entities.ResultadoCantidadProductosporcadaTienda;
-import pe.edu.upc.demotopstorefinder.entities.ResultadoTIendasPorTipoPago;
-import pe.edu.upc.demotopstorefinder.entities.TipoPago;
 import pe.edu.upc.demotopstorefinder.serviceinterfaces.IProductoService;
 
 import java.util.List;
@@ -32,9 +29,6 @@ public class ProductoController {
         pService.eliminar(id);
     }
 
-    @PutMapping
-    public void actualizar(@RequestBody Producto tp){ pService.insertar(tp);}
-
     @GetMapping
     public List<Producto> listar() {
         return pService.listar();
@@ -49,8 +43,4 @@ public class ProductoController {
     public Optional<Producto> listarId(@PathVariable("id") Integer id) {
         return pService.listarId(id);
     }
-
-    @GetMapping("/buscarcantidadtiendasporproducto")
-    public List<ResultadoCantidadProductosporcadaTienda> ResultadoCantidadProductosporcadaTienda(){
-       return pService.BuscarCantidadProductosPorTienda();
 }
