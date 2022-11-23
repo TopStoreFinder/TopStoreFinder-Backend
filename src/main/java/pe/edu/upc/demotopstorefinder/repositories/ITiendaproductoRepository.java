826c1499ecaa469164fc0eba5acd1d6f763a6d4d
Kappa04 <u20191a339@upc.edu.pe>
2022-11-23 10:51:25 -0500

    Modificación

--- src/main/java/pe/edu/upc/demotopstorefinder/repositories/ITiendaproductoRepository.java
+++ src/main/java/pe/edu/upc/demotopstorefinder/repositories/ITiendaproductoRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface ITiendaproductoRepository extends JpaRepository<Tiendaproducto, Integer> {
-    @Query(value ="SELECT p.nombre_producto, p.precio_unidad, count (t.productoid) FROM tiendaproducto t inner join producto p ON p.id = t.productoid where precio_unidad > 50 GROUP BY (p.nombre_producto,p.precio_unidad))" ,nativeQuery = true)
+    @Query(value ="SELECT p.nombre_producto, p.precio_unidad, count (t.productoid) FROM tiendaproducto t inner join producto p ON p.id = t.productoid where precio_unidad > 50 GROUP BY (p.nombre_producto,p.precio_unidad)" ,nativeQuery = true)
     List<String[]> BuscarCantidadPreciosDeterminados();
 
 }
